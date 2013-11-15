100ed8fe1756c1d6ea16484b6e83171d301f3be1
Amos Stone <amos@amosarts.com>
2013-11-15 17:22:59 -0500

    Initial commit. Tested login flow.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,13 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
+  provider :myusa, ENV['MYUSA_OAUTH_PROVIDER_KEY'], ENV['MYUSA_OAUTH_PROVIDER_SECRET'],
+	  :client_options => {
+	  		:site => ENV['MYUSA_HOME'],
+	  		:token_url => "/oauth/authorize",
+	  		:ssl => {
+	  			:verify => (ENV['RAILS_ENV'] == 'development') ? false : true
+	  		}
+	  	},
+	  :scope => "profile tasks notifications"
 end
+
+OmniAuth.config.on_failure = SessionsController.action(:failure)
