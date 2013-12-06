416f521430a666eca5880ac9f6b539602d14cd4e
Amos Stone <amos@amosarts.com>
2013-12-06 15:47:10 -0500

    Updates deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :repo_url, 'https://github.com/GSA-OCSIT/helio.git'
 
 set :default_stage, 'qa'
 
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 set :deploy_to, '/var/www/helio'
 set :current_path, "#{deploy_to}/current"
@@ -19,7 +19,7 @@ set :ssh_options, { :forward_agent => true }
 set :linked_files, %w{config/database.yml config/application.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
@@ -43,59 +43,3 @@ namespace :deploy do
   after :finishing, 'deploy:cleanup'
 
 end
-
-
-
-
-# require 'bundler/capistrano'
-# require 'capistrano/ext/multistage'
-# require './config/boot'
-# require 'airbrake/capistrano'
-
-# set :stages, %w(staging staging-cgi production-cgi)
-# set :default_stage, "staging"
-
-# set :use_sudo, false
-# set :application, "mygov-account"
-# set :scm, "git"
-# set :repository, "https://github.com/GSA-OCSIT/mygov-account.git"
-# set :deploy_via, :remote_cache
-# set :branch, ENV['BRANCH'] || 'master'
-
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-
-#   desc "Symlinks the database.yml"
-#   task :symlink_db, :roles => :app do
-#     run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
-#   end
-
-#   desc "Symlinks the initializers and other config files"
-#   task :symlink_config, :roles => :app do
-#     run "ln -nfs #{deploy_to}/shared/config/environments/production.rb #{release_path}/config/environments/production.rb"
-#     run "ln -nfs #{deploy_to}/shared/config/initializers/01_mygov.rb #{release_path}/config/initializers/01_mygov.rb"
-#     run "ln -nfs #{deploy_to}/shared/config/initializers/airbrake.rb #{release_path}/config/initializers/airbrake.rb"
-#     run "ln -nfs #{deploy_to}/shared/config/initializers/recaptcha.rb #{release_path}/config/initializers/recaptcha.rb"
-#     run "ln -nfs #{deploy_to}/shared/config/resque.yml #{release_path}/config/resque.yml"
-#   end
-
-#   desc "Restart Resque Workers"
-#   task :restart_workers, :roles => :worker do
-#     run_remote_rake "resque:restart_workers"
-#   end
-# end
-
-# # set :branch, "resque_and_devise"
-# # set :scm, :none
-# # set :repository, "."
-# # set :deploy_via, :copy
-
-
-# before 'deploy:assets:precompile', 'deploy:symlink_db'
-# before 'deploy:assets:precompile', 'deploy:symlink_config'
-# after "deploy:restart", "resque:restart"
-# after "deploy:restart", "deploy:cleanup"
