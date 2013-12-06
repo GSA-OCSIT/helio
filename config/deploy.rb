aefc4b0ed58ac11f9d47b9d78ef5e18c0766ffbd
Amos Stone <amos@amosarts.com>
2013-12-06 17:01:28 -0500

    Updates deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,11 @@
 set :application, 'helio'
-set :domain,      "github.com"
-# set :repo_url, 'https://github.com//helio.git'
-set :repository,  "ssh://#{domain}/GSA-OCSIT/#{application}.git"
+set :repo_url, 'https://github.com/GSA-OCSIT/helio.git'
 
 set :default_stage, 'qa'
 
 ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-set :deploy_to, "/var/www/#{application}"
+set :deploy_to, '/var/www/helio'
 set :current_path, "#{deploy_to}/current"
 set :shared_path, "#{deploy_to}/shared"
 
@@ -18,7 +16,7 @@ set :log_level, :debug
 set :pty, true
 set :ssh_options, { :forward_agent => true }
 
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, %w{config/database.yml config/application.yml config/redis/production.conf}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
@@ -26,8 +24,13 @@ set :keep_releases, 5
 namespace :deploy do
 
   desc 'Restart application'
-  task :restart, :roles => :app do
-    run "touch #{current_release}/tmp/restart.txt"
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      execute :mkdir, current_path.join('tmp')
+      execute :touch, current_path.join('tmp/restart.txt')
+      # run "touch #{current_release}/tmp/restart.txt"
+    end
   end
 
   after :restart, :clear_cache do
