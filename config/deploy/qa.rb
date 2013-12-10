ebdfa075891f0d776e316dd4d30fecf49f8dcc8f
Amos Stone <amos@amosarts.com>
2013-12-10 17:41:13 -0500

    Reduces workers to uno for t1.micro

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -21,7 +21,7 @@ server 'helio.gsa.io', user: 'ubuntu', roles: %w{web app db}
 role :resque_worker, "helio.gsa.io"
 # role :resque_scheduler, "app_domain"
 
-set :workers, { "alerts" => 1, "queue_alerts" => 1 }
+set :workers, { "*" => 1 }
 
 set :rvm_type, :user
 set :rvm_ruby_version, '2.0.0'
